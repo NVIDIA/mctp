0995dcbad6ccb17aad821d80fdddf4105a91b443
Faizan Ali <faizana@nvidia.com>
2025-08-04 18:03:08 +0530

    Update discovery notify signal interface
    * each MCTP reactor device which is a MCTP bridge will be capable of
      sending out Discovery Notify command. Since all MCTP reactor device
      is exposed on separate interface it makes sense to generate signal
      onto those device's interface itself. This way each rector bridge
      device will have distinguised signal point path to match disocvery
      signal onto.
    
    Tested one HGXB300
    
    '''
    root@hgxb300:~# busctl monitor --match="type='signal',path_namespace='/au/com/codeconstruct/mctp1'"
    Monitoring bus message stream.
    > Type=signal  Endian=l  Flags=1  Version=1 Cookie=659  Timestamp="Mon 2025-08-04 14:31:49.311605 UTC"
      Sender=:1.11949  Path=/au/com/codeconstruct/mctp1/interfaces/mctpusb12  Interface=au.com.codeconstruct.MCTP.BusOwner1  Member=DiscoveryNotify
      UniqueName=:1.11949
      MESSAGE "u" {
              UINT32 80;
      };
    
    > Type=signal  Endian=l  Flags=1  Version=1 Cookie=660  Timestamp="Mon 2025-08-04 14:31:49.312250 UTC"
      Sender=:1.11949  Path=/au/com/codeconstruct/mctp1/interfaces/mctpusb11  Interface=au.com.codeconstruct.MCTP.BusOwner1  Member=DiscoveryNotify
      UniqueName=:1.11949
      MESSAGE "u" {
              UINT32 79;
      };
    
    > Type=signal  Endian=l  Flags=1  Version=1 Cookie=661  Timestamp="Mon 2025-08-04 14:31:49.312488 UTC"
      Sender=:1.11949  Path=/au/com/codeconstruct/mctp1/interfaces/mctpusb4  Interface=au.com.codeconstruct.MCTP.BusOwner1  Member=DiscoveryNotify
      UniqueName=:1.11949
      MESSAGE "u" {
              UINT32 74;
      };
    
    > Type=signal  Endian=l  Flags=1  Version=1 Cookie=662  Timestamp="Mon 2025-08-04 14:31:49.312709 UTC"
      Sender=:1.11949  Path=/au/com/codeconstruct/mctp1/interfaces/mctpusb5  Interface=au.com.codeconstruct.MCTP.BusOwner1  Member=DiscoveryNotify
      UniqueName=:1.11949
      MESSAGE "u" {
              UINT32 77;
      };
    
      Aug 04 14:33:47 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '76'.
    Aug 04 14:33:47 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '75'.
    Aug 04 14:33:47 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '80'.
    Aug 04 14:33:47 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '78'.
    Aug 04 14:33:47 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '77'.
    Aug 04 14:33:50 hgxb300 mctpreactor[809]: First DiscoveryNotify for EID '69' on interface 'mctpusb0' => scheduling discovery in ~5s.
    Aug 04 14:33:50 hgxb300 mctpreactor[809]: First DiscoveryNotify for EID '71' on interface 'mctpusb4' => scheduling discovery in ~5s.
    Aug 04 14:33:50 hgxb300 mctpreactor[809]: First DiscoveryNotify for EID '72' on interface 'mctpusb7' => scheduling discovery in ~5s.
    Aug 04 14:33:50 hgxb300 mctpreactor[809]: First DiscoveryNotify for EID '70' on interface 'mctpusb1' => scheduling discovery in ~5s.
    Aug 04 14:33:56 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '69'.
    Aug 04 14:33:56 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '71'.
    Aug 04 14:33:56 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '72'.
    Aug 04 14:33:56 hgxb300 mctpreactor[809]: Successfully called GetRoutingTable for EID '70'.
    root@hgxb300:~#
    
    '''
    
    Fixes JIRA https://upstream_sync
    
    Signed-off-by: Faizan Ali <faizana@nvidia.com>

--- src/mctpd.c
+++ src/mctpd.c
@@ -871,8 +871,25 @@ static int handle_control_discovery_notify(struct ctx *ctx, int sd,
 		       ext_addr_tostr(addr));
 	}
 
-	int r = sd_bus_emit_signal(ctx->bus, MCTP_DBUS_PATH,
-				   CC_MCTP_DBUS_IFACE_ENDPOINT,
+	struct peer *peer =
+		find_peer_by_addr(ctx, addr->smctp_base.smctp_addr.s_addr,
+				  addr->smctp_base.smctp_network);
+	if (!peer) {
+		warnx("Unknown EID %d received Discovery notify",
+		      addr->smctp_base.smctp_addr.s_addr);
+		return -1;
+	}
+	const char *ifname =
+		mctp_nl_if_byindex(peer->ctx->nl, peer->phys.ifindex);
+	if (!ifname) {
+		warnx("No ifname found for eid %d",
+		      addr->smctp_base.smctp_addr.s_addr);
+		return -1;
+	}
+	char path[256];
+	snprintf(path, sizeof(path), "%s/%s", MCTP_DBUS_PATH_LINKS, ifname);
+
+	int r = sd_bus_emit_signal(ctx->bus, path, CC_MCTP_DBUS_IFACE_BUSOWNER,
 				   "DiscoveryNotify", "u",
 				   addr->smctp_base.smctp_addr.s_addr);
 	if (r < 0) {
